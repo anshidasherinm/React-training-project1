d38517adb55cb04966e4f3355ed1b375bf0abf35
anshida-m <anshida@litmus7.com>
2022-12-09 13:57:24 +0530

    updated using useReducer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
-import { useState, useEffect } from "react";
+import { useState, useReducer } from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodosContainer from "./components/TodosContainer";
 
 import Card from "./ui/Card";
 
-function App() {
-  const [todoList, setTodoList] = useState([
+const initialtodoListState = {
+  todoList: [
     { id: 1, title: "Buy Groceries", checked: false },
     { id: 2, title: "Go to Gym", checked: false },
     { id: 3, title: "Plan next task", checked: true },
     { id: 4, title: "Another task", checked: false },
-  ]);
+  ],
+};
+const todoListReducer = (prevState, action) => {
+  if (action.type === "ADD") {
+    return { todoList: [...prevState.todoList, action.val] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      todoList: prevState.todoList.filter((todo) => {
+        return todo.id !== action.id;
+      }),
+    };
+  }
 
-  // const todos = [
-  //   { id: 1, title: "react study", checked: false },
-  //   { id: 2, title: "compleet project", checked: false },
-  // ];
+  if (action.type === "CHANGE_STATUS") {
+    return {
+      todoList: prevState.todoList.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, checked: !todo.checked };
+        }
+        return todo;
+      }),
+    };
+  }
+
+  return {
+    initialtodoListState,
+  };
+};
+function App() {
+  // const [todoList, setTodoList] = useState([
+  //   { id: 1, title: "Buy Groceries", checked: false },
+  //   { id: 2, title: "Go to Gym", checked: false },
+  //   { id: 3, title: "Plan next task", checked: true },
+  //   { id: 4, title: "Another task", checked: false },
+  // ]);
+  const [todoListState, dispatchtodoList] = useReducer(
+    todoListReducer,
+    initialtodoListState
+  );
 
   const addTodoHandler = (todo) => {
-    setTodoList((prev) => [...prev, todo]);
+    // setTodoList((prev) => [...prev, todo]);
+    dispatchtodoList({ type: "ADD", val: todo });
   };
 
   const removeTodoHandler = (id) => {
-    setTodoList((prev) =>
-      prev.filter((todo) => {
-        return todo.id !== id;
-      })
-    );
+    // setTodoList((prev) =>
+    //   prev.filter((todo) => {
+    //     return todo.id !== id;
+    //   })
+    // );
+    dispatchtodoList({ type: "REMOVE", id: id });
   };
 
   // useEffect(() => {
@@ -35,29 +71,24 @@ function App() {
   // }, [todoList]);
 
   const addCheckHandler = (id) => {
-    // const newTodoList = todoList;
-    // for (var key of newTodoList) {
-    //   if (key.id === status.id) {
-    //     key.checked = status.checked;
+    dispatchtodoList({ type: "CHANGE_STATUS", id: id });
+    // const newTodoList = todoList.map((todo) => {
+    //   if (todo.id === id) {
+    //     return { ...todo, checked: !todo.checked };
     //   }
-    // }
+    //   return todo;
+    // });
     // setTodoList(newTodoList);
-    // console.log(todoList);
-    const newTodoList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, checked: !todo.checked };
-      }
-      return todo;
-    });
-    setTodoList(newTodoList);
   };
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <Card>
         <TodoForm addTodo={addTodoHandler} />
         <TodosContainer
-          todos={todoList}
+          //  todos={todoList}
+          todos={todoListState.todoList}
           removeTodoHandler={removeTodoHandler}
           addCheck={addCheckHandler}
         />
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,43 +1,66 @@
-import React, { useState } from "react";
+import React, { useRef, useReducer } from "react";
 import Button from "../ui/Button";
 import classes from "./TodoForm.module.css";
+
+const initialtodoState = {
+  title: "",
+  isValid: true,
+};
+
+const todoReducer = (prevState, action) => {
+  if (action.type === "SET") {
+    return { title: action.val, isValid: action.val.trim().length > 0 };
+  }
+  if (action.type === "RESET") {
+    return { title: "", isValid: true };
+  }
+  if (action.type === "INVALID") {
+    return { title: "", isValid: false };
+  }
+  return {
+    initialtodoState,
+  };
+};
+
 const TodoForm = (props) => {
-  const [item, setItem] = useState("");
-  const [isValid, setIsValid] = useState(true);
+  const todoRef = useRef();
+
+  const [todoState, dispatchtodo] = useReducer(todoReducer, initialtodoState);
+
   const setItemHandler = (event) => {
-    setItem(event.target.value);
+    dispatchtodo({ type: "SET", val: event.target.value });
   };
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    // console.log("submitted");
-    if (item.trim().length === 0) {
-      setIsValid(false);
-      return;
+
+    if (todoRef.current.value) {
+      const newTodo = {
+        id: Math.trunc(Math.random() * 2000),
+        title: todoState.title,
+        checked: false,
+      };
+      props.addTodo(newTodo);
+      dispatchtodo({ type: "RESET" });
+    } else {
+      dispatchtodo({ type: "INVALID" });
     }
-    setIsValid(true);
-    const newTodo = {
-      id: Math.trunc(Math.random() * 2000),
-      title: item,
-      checked: false,
-    };
-    // console.log(newTodo);
-    props.addTodo(newTodo);
-    setItem("");
   };
   return (
     <div className={classes.formcontainer}>
       <form className={classes.form} onSubmit={formSubmitHandler}>
         <input
+          ref={todoRef}
           type="text"
           placeholder="new todo item"
-          value={item}
+          value={todoState.title}
           onChange={setItemHandler}
         />
 
         <Button className={classes.button} type="submit" />
       </form>
       <span className={classes.errormsg}>
-        {!isValid && "Please add a todo !"}
+        {/* {!isValid && "Please add a todo !"} */}
+        {!todoState.isValid && "Please add a todo !"}
       </span>
     </div>
   );
