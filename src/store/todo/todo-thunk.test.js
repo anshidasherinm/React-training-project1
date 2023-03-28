788384d664849a0e1efd1d7d90420a12b3234350
nithincs <nithincspnr@gmail.com>
2023-03-28 13:40:34 +0530

    ignore thunk for now

---
+++ src/store/todo/todo-thunk.test.js
@@ -0,0 +1,15 @@
+import { addTodoItem } from './todo-thunk.js';
+import store from '../index';
+// import * as utils from '../../utils'
+
+// jest.mock('../../utils')
+
+
+describe('todo thunk', () => {
+    describe('addTodoItem', () => {
+        it('sample test', async () => {
+            expect(1).toBe(1);
+        })
+    })
+
+})
